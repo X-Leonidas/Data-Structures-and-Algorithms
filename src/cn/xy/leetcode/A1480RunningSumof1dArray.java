78bc8463a3d5178f3b685ce16a75b86715699d8a
xiangyu <jokersxy@163.com>
2020-10-30 17:25:25 +0800

    1480-一维数组动态和-简单

---
+++ src/cn/xy/leetcode/A1480RunningSumof1dArray.java
@@ -0,0 +1,57 @@
+package cn.xy.leetcode;
+
+/**
+ * @author XiangYu
+ * @create2020-10-30-17:16
+ * 给你一个数组 nums 。数组「动态和」的计算公式为：runningSum[i] = sum(nums[0]…nums[i]) 。
+ *
+ * 请返回 nums 的动态和。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,2,3,4]
+ * 输出：[1,3,6,10]
+ * 解释：动态和计算过程为 [1, 1+2, 1+2+3, 1+2+3+4] 。
+ * 示例 2：
+ *
+ * 输入：nums = [1,1,1,1,1]
+ * 输出：[1,2,3,4,5]
+ * 解释：动态和计算过程为 [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1] 。
+ * 示例 3：
+ *
+ * 输入：nums = [3,1,2,10,1]
+ * 输出：[3,4,6,16,17]
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 1000
+ * -10^6 <= nums[i] <= 10^6
+ */
+public class A1480RunningSumof1dArray {
+
+    //自己的写法
+    public int[] runningSum(int[] nums) {
+        int sum  = 0;
+        int[] sums = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+            sums[i] = sum;
+        }
+
+        return  sums;
+    }
+
+    //看解题思路写法
+    public int[] runningSum2(int[] nums) {
+
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i-1] + nums[i];
+        }
+
+        return nums;
+    }
+}
