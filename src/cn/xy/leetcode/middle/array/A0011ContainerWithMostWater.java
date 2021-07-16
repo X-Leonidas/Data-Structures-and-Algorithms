9ad09d5503cc4abd509495d3bb06319a9fb7b49c
xiangyu <xiangyujava@163.com>
2021-07-17 00:19:27 +0800

    日常提交

--- src/cn/xy/leetcode/middle/array/A0011ContainerWithMostWater.java
+++ src/cn/xy/leetcode/middle/array/A0011ContainerWithMostWater.java
@@ -1,8 +1,71 @@
 package cn.xy.leetcode.middle.array;
 
+import sun.security.util.Length;
+
+import java.awt.event.HierarchyBoundsAdapter;
+import java.util.Arrays;
+
 /**
  * @author XiangYu
- * @create2021-04-26-21:37
+ * @Create 2021年7月16日23点35分
+ * <p>
+ * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。
+ * 在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0) 。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ * 输入：[1,8,6,2,5,4,8,3,7]
+ * 输出：49
+ * 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：height = [1,1]
+ * 输出：1
+ * 示例 3：
+ * <p>
+ * 输入：height = [4,3,2,1,4]
+ * 输出：16
+ * 示例 4：
+ * <p>
+ * 输入：height = [1,2,1]
+ * 输出：2
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * n = height.length
+ * 2 <= n <= 3 * 104
+ * 0 <= height[i] <= 3 * 104
  */
 public class A0011ContainerWithMostWater {
+
+    public static void main(String[] args) {
+        int[] ints = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        maxArea(ints);
+    }
+
+    /**
+     * 使用双指针，每次移动的指针的条件判断为：移动当前数值较小的指针，因为如果移动大的，不可能出现更大的面积
+     * @param height
+     * @return
+     */
+    public static int maxArea(int[] height) {
+        if (height == null || height.length == 1) {
+            return 0;
+        }
+
+        int length = height.length;
+        int max = -1;
+        int start = 0;
+        int end = length - 1;
+
+        while (start < end) {
+            max = Math.max(max,Math.min(height[start], height[end]) * (end - start));
+            if (height[end] > height[start]) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+        return max;
+    }
 }
--- src/cn/xy/leetcode/middle/array/A0031NextPermutation.java
+++ src/cn/xy/leetcode/middle/array/A0031NextPermutation.java
@@ -1,3 +1,61 @@
+package cn.xy.leetcode.middle.array;
+
+/**
+ * 注意到下一个排列总是比当前排列要大，除非该排列已经是最大的排列。我们希望找到一种方法，能够找到一个大于当前序列的新序列，且变大的幅度尽可能小。具体地：
+ *
+ * 我们需要将一个左边的「较小数」与一个右边的「较大数」交换，以能够让当前排列变大，从而得到下一个排列。
+ *
+ * 同时我们要让这个「较小数」尽量靠右，而「较大数」尽可能小。当交换完成后，「较大数」右边的数需要按照升序重新排列。这样可以在保证新排列大于原来排列的情况下，使变大的幅度尽可能小。
+ *
+ * 以排列 [4,5,2,6,3,1][4,5,2,6,3,1] 为例：
+ *
+ * 我们能找到的符合条件的一对「较小数」与「较大数」的组合为 22 与 33，满足「较小数」尽量靠右，而「较大数」尽可能小。
+ *
+ * 当我们完成交换后排列变为 [4,5,3,6,2,1][4,5,3,6,2,1]，此时我们可以重排「较小数」右边的序列，序列变为 [4,5,3,1,2,6][4,5,3,1,2,6]。
+ */
 public class A0031NextPermutation{
-  //TODO:https://leetcode-cn.com/problems/next-permutation/
+    /**
+     *
+     * @param nums
+     */
+    public void nextPermutation(int[] nums) {
+        int length = nums.length;
+        if (length <= 1) {
+            return;
+        }
+        int little = -1;
+        int bigger = 0;
+
+        for (int i = length-1; i >= 1; i--) {
+            if(nums[i-1] < nums[i]){
+                little = i-1;
+                break;
+            }
+        }
+        if(little != -1){
+            for(int i = length-1; i >= little; i --){
+                if(nums[i] > nums[little]){
+                    bigger = i;
+                    break;
+                }
+            }
+
+            swap(nums,bigger,little);
+        }
+        //双指针重新排列后续数据，使得数字尽量的小
+        reverse(nums,little+1,length-1);
+    }
+    private static void swap(int[] nums,int index1, int index2){
+        int temp  = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+
+    private  static void reverse(int[] nums,int start, int end){
+        while(start < end){
+            swap(nums,start,end);
+            start++;
+            end--;
+        }
+    }
 }
