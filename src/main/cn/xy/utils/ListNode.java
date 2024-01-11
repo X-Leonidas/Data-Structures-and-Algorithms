d36e72de3ed6148d13aa90b1658844719ae51c34
xiangyu <xiangyujava@163.com>
2024-01-11 20:46:06 +0800

    @dev

--- src/main/cn/xy/utils/ListNode.java
+++ src/main/cn/xy/utils/ListNode.java
@@ -3,23 +3,31 @@ package cn.xy.utils;
 import java.util.ArrayList;
 
 public  class ListNode {
-      public  int val;
+      public int val;
       public ListNode next;
-      public ListNode(int x) { val = x; }
-      public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+      public ListNode(int x) {
+            val = x;
+      }
+
+      public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+      }
 
 
-      public static ListNode getDemo(int[] array){
+      public static ListNode getDemo(int[] array) {
             ArrayList<ListNode> listNodes = new ArrayList<>();
-            for (int i = 0; i < array.length; i++) {
-                  listNodes.add(new ListNode(array[i]));
+          for (int j : array) {
+              listNodes.add(new ListNode(j));
+          }
+
+
+            for (int i = 0; i < listNodes.size() - 1; i++) {
+                  listNodes.get(i).next = listNodes.get(i + 1);
             }
 
-
-            for (int i = 0; i < listNodes.size() -1; i++) {
-                  listNodes.get(i).next = listNodes.get(i+1);
-            }
-
-            return  listNodes.get(0);
+            return listNodes.get(0);
       }
 }
+
