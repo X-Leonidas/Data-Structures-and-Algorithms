f206bfff82929c7759793a7e45c1f3b1f65adfeb
xiangyu <xiangyujava@163.com>
2024-02-29 00:40:18 +0800

    feat : 最近写的一些题

---
+++ src/main/cn/xy/leetcode/middle/array/A0189RotateArray.java
@@ -0,0 +1,93 @@
+package cn.xy.leetcode.middle.array;
+
+/**
+ * @author xiangyu
+ * @date 2024-01-25 23:45
+ * 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+ * 示例 1:
+ * <p>
+ * 输入: nums = [1,2,3,4,5,6,7], k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右轮转 1 步: [7,1,2,3,4,5,6]
+ * 向右轮转 2 步: [6,7,1,2,3,4,5]
+ * 向右轮转 3 步: [5,6,7,1,2,3,4]
+ * 示例 2:
+ * <p>
+ * 输入：nums = [-1,-100,3,99], k = 2
+ * 输出：[3,99,-1,-100]
+ * 解释:
+ * 向右轮转 1 步: [99,-1,-100,3]
+ * 向右轮转 2 步: [3,99,-1,-100]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 105
+ * -231 <= nums[i] <= 231 - 1
+ * 0 <= k <= 105
+ * <p>
+ * <p>
+ * 进阶：
+ * <p>
+ * 尽可能想出更多的解决方案，至少有 三种 不同的方法可以解决这个问题。
+ * 你可以使用空间复杂度为 O(1) 的 原地 算法解决这个问题吗？
+ */
+public class A0189RotateArray {
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3,4,5,6,7};
+        new A0189RotateArray().rotate(nums,3);
+    }
+    public  void rotate(int[] nums, int k) {
+        int n = nums.length;
+        // 要把多少个从末尾移到数组头部
+        k = k % n;
+        int count = gcd(k, n);
+        for (int start = 0; start < count; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % n;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+            } while (start != current);
+        }
+    }
+
+    /**
+     * 最大公约数
+     * @param k
+     * @param numsLength
+     * @return
+     */
+    public int gcd(int k, int numsLength) {
+        return numsLength > 0 ? gcd(numsLength, k % numsLength) : k;
+    }
+
+    /**
+     * 颠倒数组
+     * @param nums
+     * @param k
+     */
+    public void rotate2(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start += 1;
+            end -= 1;
+        }
+    }
+
+}
