02a0779a2f014b6eb65d464c7a8b37bf093e4841
xiangyu <xiangyujava@163.com>
2024-12-05 23:01:48 +0800

    feat : 日常提交

---
+++ src/main/cn/xy/leetcode/middle/array/A0080RemoveDuplicatesFromSortedArrayII.java
@@ -0,0 +1,41 @@
+package cn.xy.leetcode.middle.array;
+
+/**
+ * @author xiangyu
+ * @date 2024-12-05 21:56
+ * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使得出现次数超过两次的元素只出现两次 ，返回删除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ * <p>
+ * 示例 1：
+ * 输入：nums = [1,1,1,2,2,3]
+ * 输出：5, nums = [1,1,2,2,3]
+ * 解释：函数应返回新长度 length = 5, 并且原数组的前五个元素被修改为 1, 1, 2, 2, 3。 不需要考虑数组中超出新长度后面的元素。
+ * <p>
+ * 示例 2：
+ * 输入：nums = [0,0,1,1,1,1,2,3,3]
+ * 输出：7, nums = [0,0,1,1,2,3,3]
+ * 解释：函数应返回新长度 length = 7, 并且原数组的前七个元素被修改为 0, 0, 1, 1, 2, 3, 3。不需要考虑数组中超出新长度后面的元素。
+ * <p>
+ * 提示：
+ * 1 <= nums.length <= 3 * 104
+ * -104 <= nums[i] <= 104
+ * nums 已按升序排列
+ */
+public class A0080RemoveDuplicatesFromSortedArrayII {
+
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return nums.length;
+        }
+        int fast = 2, slow = 2;
+        while (fast < n) {
+            if (nums[fast] != nums[slow - 2]) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+}
