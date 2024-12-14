ff8fb4cd4cec3445ea19de31924bac0d2e37d0c6
xiangyu <xiangyujava@163.com>
2024-12-14 21:23:35 +0800

    feat : 日常练习

--- src/main/cn/xy/leetcode/middle/array/A0238ProductOfArrayExceptSelf.java
+++ src/main/cn/xy/leetcode/middle/array/A0238ProductOfArrayExceptSelf.java
@@ -6,27 +6,20 @@ package cn.xy.leetcode.middle.array;
  * 给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
  * <p>
  * 题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
- * <p>
  * 请 不要使用除法，且在 O(n) 时间复杂度内完成此题。
  * <p>
- * <p>
- * <p>
  * 示例 1:
- * <p>
  * 输入: nums = [1,2,3,4]
  * 输出: [24,12,8,6]
+ *
  * 示例 2:
- * <p>
  * 输入: nums = [-1,1,0,-3,3]
  * 输出: [0,0,9,0,0]
  * <p>
- * <p>
  * 提示：
- * <p>
  * 2 <= nums.length <= 105
  * -30 <= nums[i] <= 30
  * 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内
- * <p>
  * <p>
  * 进阶：你可以在 O(1) 的额外空间复杂度内完成这个题目吗？（ 出于对空间复杂度分析的目的，输出数组 不被视为 额外空间。）
  */
@@ -73,9 +66,15 @@ public class A0238ProductOfArrayExceptSelf {
 
         int[] ans = new int[nums.length];
         ans[0] = 1;
+        // 先让 ans[i] = nums[0] * .... * nums[i-1]
         for (int i = 1; i < nums.length; i++) {
             ans[i] = ans[i - 1] * nums[i - 1];
         }
+
+
+        // 从后往前 遍历 nums，补齐 i 之后的乘积
+        //从 nums.length - 2 开始 是因为 最后一个元素之后没有需要补齐的
+        // 代表 从 最后一位到当前位的乘积
         int temp = 1;
         for (int i = nums.length - 2; i >= 0; i--) {
             temp = temp * nums[i + 1];
